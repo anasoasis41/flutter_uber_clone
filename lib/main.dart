c895c11777f27399ed7cdaa1e62d226d16969c1a
Anas Riahi <anas.riahi@smile-maroc.com>
2019-05-13 13:48:17 +0000

    Add google maps api to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() => runApp(MyApp());
 
@@ -30,10 +31,10 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(initialCameraPosition: CameraPosition(target: LatLng(31.6295, 7.9811),zoom: 5),)
+        ],
       ),
     );
   }
