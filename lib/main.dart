31aab6902434b2b37dd8b256e5df664949bdd558
Anas Riahi <anas.riahi@smile-maroc.com>
2019-05-14 15:59:43 +0000

    Add current position to maps

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import 'requests/google_maps_requests.dart';
 import 'utils/core.dart';
 
 void main() => runApp(MyApp());
@@ -9,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Uber clone',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -44,13 +47,28 @@ class Map extends StatefulWidget {
 
 class _MapState extends State<Map> {
   GoogleMapController mapController;
-  static const _initialPosition = LatLng(33.596604,-7.638373);
+  GoogleMapsServices googleMapsServices = GoogleMapsServices();
+  TextEditingController locationController = TextEditingController();
+  TextEditingController destinationController = TextEditingController();
+  static LatLng _initialPosition;
   LatLng _lastPosition = _initialPosition;
   final Set<Marker> _marker = {};
+  final Set<Polyline> _polylines = {};
+
+  @override
+  void initState() {
+    super.initState();
+    getUserLocation();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
+    return _initialPosition == null? Container(
+      alignment: Alignment.center,
+      child: Center(
+        child: CircularProgressIndicator(),
+      ),
+    ) : Stack(
       children: <Widget>[
         GoogleMap(
           initialCameraPosition: CameraPosition(target: _initialPosition, zoom: 15),
@@ -62,6 +80,67 @@ class _MapState extends State<Map> {
         ),
 
         Positioned(
+          top: 50.0,
+          right: 15.0,
+          left: 15.0,
+          child: Container(
+            height: 50.0,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(3.0),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey,
+                    offset: Offset(1.0, 5.0),
+                    blurRadius: 10,
+                    spreadRadius: 3)
+              ],
+            ),
+            child: TextField(
+              cursorColor: Colors.black,
+              controller: locationController,
+              decoration: InputDecoration(
+                icon: Container(margin: EdgeInsets.only(left: 20, top: 5), width: 10, height: 10, child: Icon(Icons.location_on, color: Colors.black,),),
+                hintText: "pick up",
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(left: 15.0, top: 16.0),
+              ),
+            ),
+          ),
+        ),
+
+        Positioned(
+          top: 105.0,
+          right: 15.0,
+          left: 15.0,
+          child: Container(
+            height: 50.0,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(3.0),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey,
+                    offset: Offset(1.0, 5.0),
+                    blurRadius: 10,
+                    spreadRadius: 3)
+              ],
+            ),
+            child: TextField(
+              cursorColor: Colors.black,
+              decoration: InputDecoration(
+                icon: Container(margin: EdgeInsets.only(left: 20, top: 5), width: 10, height: 10, child: Icon(Icons.local_taxi, color: Colors.black,),),
+                hintText: "destination?",
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(left: 15.0, top: 16.0),
+              ),
+            ),
+          ),
+        ),
+
+        /*Positioned(
           top: 40,
           right: 10,
           child: FloatingActionButton(onPressed: _onAddMarkerPressed,
@@ -69,7 +148,7 @@ class _MapState extends State<Map> {
             backgroundColor: black,
             child: Icon(Icons.add_location, color: white),
           ),
-        )
+        )*/
       ],
     );
   }
@@ -95,6 +174,67 @@ class _MapState extends State<Map> {
           icon: BitmapDescriptor.defaultMarker
       ));
     });
+  }
+
+  // convert list of doubles into LatLng
+  List<LatLng> convertToLatLng(List points) {
+    List<LatLng> result = <LatLng>[];
+    for (int i = 0; i < points.length; i++) {
+      if (i % 2 != 0) {
+        result.add(LatLng(points[i - 1], points[i]));
+      }
+    }
+    return result;
+  }
+
+  List decodePoly(String poly){
+    var list=poly.codeUnits;
+    var lList=new List();
+    int index=0;
+    int len= poly.length;
+    int c=0;
+    // repeating until all attributes are decoded
+    do
+    {
+      var shift=0;
+      int result=0;
+
+      // for decoding value of one attribute
+      do
+      {
+        c=list[index]-63;
+        result|=(c & 0x1F)<<(shift*5);
+        index++;
+        shift++;
+      }while(c>=32);
+      /* if value is negetive then bitwise not the value */
+      if(result & 1==1)
+      {
+        result=~result;
+      }
+      var result1 = (result >> 1) * 0.00001;
+      lList.add(result1);
+    }while(index<len);
+
+    /*adding to previous value as done in encoding */
+    for(var i=2;i<lList.length;i++)
+      lList[i]+=lList[i-2];
+
+    print(lList.toString());
+
+    return lList;
+  }
+
+  void getUserLocation() async{
+    // Getting current position of the device
+    Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    // Get the placemarks
+    List<Placemark> placemark = await Geolocator().placemarkFromCoordinates(position.latitude, position.longitude);
+    setState(() {
+      _initialPosition = LatLng(position.latitude, position.longitude);
+      locationController.text = placemark[0].name;
+    });
+
   }
 }
 
